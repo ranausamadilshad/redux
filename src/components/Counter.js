95e2032994230408fb5c1782e5729daf09aed8e8
ranausamadilshad <ranausamaofficial11@gmail.com>
2021-12-29 20:51:23 +0500

    Attaching Payloads to actions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,33 @@
 import classes from './Counter.module.css';
-import {useSelector} from "react-redux"
+import {useSelector,useDispatch} from "react-redux"
 
 const Counter = () => {
+  const dispatch=useDispatch();
+  const counter = useSelector(state => state.counter);
+  const incrementHandler = () => {
+    dispatch({type:"increment"})
+  }
+   const decrementHandler = () => {
+    dispatch({type:"decrement"})
+  }
 
-  const counter=useSelector(state=>state.counter);
+    const increaseBy5 = () => {
+    dispatch({type:"increase",amount:5})
+  }
   const toggleCounterHandler = () => {};
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{ counter}</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+      <button style={{margin:"1rem"}} onClick={incrementHandler}>Increment</button>
+        <button style={{ margin: "1rem" }} onClick={decrementHandler}>Decrement</button>
+      <button style={{margin:"1rem"}} onClick={increaseBy5}>Increase By 5</button>
+        
+      </div> 
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+
+
     </main>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,11 @@ const counterReducer = (state= {counter : 0},action) => {
             counter : state.counter - 1
         }
     }
+      if (action.type === "increase") {
+        return {
+            counter : state.counter  + action.amount
+        }
+    }
     return state
 }
 
